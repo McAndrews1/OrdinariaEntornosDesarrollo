c2ca16d9f70ed9e0d86b08e91eba199c865df8d4
McAndrews1 <lgmcandrews@gmail.com>
2020-06-23 04:21:47 +0200

    en esta version se ha actualizado la clase doc, se ha creado un metodo nuevo samppleMethod

--- doc.java
+++ doc.java
@@ -3,19 +3,26 @@
  * Devuelve el contenido de la fila x del fichero de texto con ruta absoluta file
  * 
  * @author (Mykola Oliinyk) 
- * @version (1.1)
+ * @version (1.2)
  */
 public interface doc
 {
-   
-   // Devuelve el contenido de la fila x del fichero de texto con ruta absoluta file.
-   /**
-    * Devuelve el contenido de la fila x del fichero de texto con ruta absoluta file
-    * @param  x  numero de la fila
-    *         file  fichero de texto
-    */
-   public abstract String obtenerLinea(int x, String file);
-   
-    
+
+    // Devuelve el contenido de la fila x del fichero de texto con ruta absoluta file.
+    /**
+     * Devuelve el contenido de la fila x del fichero de texto con ruta absoluta file
+     * @param  x  numero de la fila
+     *         file  fichero de texto
+     * @return  contenido de la fila correspondiengte
+     */
+    public abstract String obtenerLinea(int x, String file);
+
+    /**
+     *  Devuelve la posición geográfica (latitud y altitud) dado una dirección
+     *
+     * @param  direccion  es una cadena que representa la direccion de una calla o un sitio en el campo etc....
+     * @return    posicion geografica
+     */
+    public abstract int sampleMethod(String direccion);
 
 }
