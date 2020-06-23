859a20a4beb196071bc9b14ae08debf4f1018fb5
McAndrews1 <lgmcandrews@gmail.com>
2020-06-23 04:12:47 +0200

    En esta version se ha actualizado la documentacion de la clase doc

--- doc.java
+++ doc.java
@@ -1,15 +1,21 @@
 
 /**
- * Write a description of class doc here.
+ * Devuelve el contenido de la fila x del fichero de texto con ruta absoluta file
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Mykola Oliinyk) 
+ * @version (1.1)
  */
 public interface doc
 {
    
    // Devuelve el contenido de la fila x del fichero de texto con ruta absoluta file.
+   /**
+    * Devuelve el contenido de la fila x del fichero de texto con ruta absoluta file
+    * @param  x  numero de la fila
+    *         file  fichero de texto
+    */
    public abstract String obtenerLinea(int x, String file);
    
-   
+    
+
 }
